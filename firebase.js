9b11461c1d2d9ee2c70f607326612f4d4ac16804
Morgansenpai <166062389+Morgansenpai@users.noreply.github.com>
2026-09-24 18:47:05 +0100

    Initialize Firebase app and Firestore database

--- firebase.js
+++ firebase.js
@@ -1,1 +1,15 @@
+import { initializeApp }
+from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDP4DiJpagDIXz8gGCrUAbMxhgF-HzGvyw",
+  authDomain: "hey-you-companion.firebaseapp.com",
+  projectId: "hey-you-companion",
+  storageBucket: "hey-you-companion.appspot.com",
+  messagingSenderId: "1061661966821",
+  appId: "1:1061661966821:web:e4599453150421b53fef0c"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
